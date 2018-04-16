6ec141d984766918b9a0ef4ae7311e6e5fac4834
Sisillia <sisilliatricahyani@yahoo.com>
2018-04-16 18:30:26 +0800

    final commit

---
+++ app/src/main/java/com/example/lenovog40/androiddev/contoh.java
@@ -0,0 +1,13 @@
+package com.example.lenovog40.androiddev;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class contoh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contoh);
+    }
+}
